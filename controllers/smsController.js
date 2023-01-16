72c067ed6df51ca4445137ff632544dd110d2a5b
code <victorkbuzes@gmail.com>
2023-01-16 15:29:22 +0300

    first commit

---
+++ controllers/smsController.js
@@ -0,0 +1,38 @@
+// Define application constants
+const message = 'Premium SMS SHortCode'
+// Your login credentials
+const shortCode = '32192'
+const username = 'sandbox'
+const apikey = '4fc63b75c4d39db96e4895fa652a74a8399a1777339d1a777e4d236908d3b6b5'
+const options = {
+apiKey: apikey,
+username: username
+}
+
+
+const AfricasTalking = require('africastalking')(options)
+const sms = AfricasTalking.SMS
+
+
+exports.received = (req, res) => {
+    var body = req.body;
+    if (body.to == '32192') {
+        sendResponse(body.from, message)
+        
+    }
+
+}
+
+
+function sendResponse (recipient, message) {
+    var opts = {
+    from: shortCode,
+    to: recipient,
+    message: message
+    }
+    sms.send(opts).then(
+    console.log('Message sent successfully')
+    ).catch(
+    console.log('Something went wrong with message sending')
+    )
+    }
---
+++ index.js
@@ -0,0 +1,34 @@
+
+require("dotenv").config();
+const express = require("express");
+var path = require('path');
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+
+// const indexRouter = require('./routes/Index')
+const smsRoter = require('./routes/Sms')
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/',indexRouter)
+app.use('/sms', smsRoter);
+
+
+    // app.use(cors());
+
+    const PORT =  process.env.PORT || 8080
+    
+    
+    // ussd setup
+    
+    
+        
+    app.listen(PORT, () => {
+       
+        console.log(`running port ${PORT}`)
+    });
+    
