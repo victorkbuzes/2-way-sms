a1efcaf07dbaa71a73b2a8c0ccc4c2d657abc151
code <victorkbuzes@gmail.com>
2023-01-17 13:17:53 +0300

    code

--- routes/Sms.js
+++ routes/Sms.js
@@ -3,4 +3,9 @@ var router = express.Router();
 var sms = require('../controllers/smsController')
 
 router.post('/', sms.received)
+router.get("/welcome", function (req, res) {
+    res.send("Welcome")
+    
+})
+
 module.exports = router
